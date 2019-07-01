1c71ea4337f2f05cf2c2c7a29ccae246ad54f0b8
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-07-01 19:41:07 -0400

    Fixed and completed question 20

--- jason/jmacias-java-core-assingment/src/main/java/com/revature/core_java_assignment_jmacias/Driver.java
+++ jason/jmacias-java-core-assingment/src/main/java/com/revature/core_java_assignment_jmacias/Driver.java
@@ -247,7 +247,16 @@ public class Driver {
 		System.out.println("");
 		
 		
-		// Q20 - 
+		// Q20 - Create a notepad file called Data.txt and enter the following:
+//		Mickey:Mouse:35:Arizona
+//		Hulk:Hogan:50:Virginia
+//		Roger:Rabbit:22:California
+//		Wonder:Woman:18:Montana
+//		Write a program that would read from the file and print it out to the screen in the following
+//		format:
+//		Name: Mickey Mouse
+//		Age: 35 years
+//		State: Arizona State
 		
 		Q20.readData();
 		
